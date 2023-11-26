768ab6e1e464a4f49249d52411742c017287c802
M.Ismail <miktv101@gmail.com>
2023-11-25 19:05:40 -0800

    RubricCreate

---
+++ scoremaster Presentation/Models/RubricCreate.cs
@@ -0,0 +1,11 @@
+﻿namespace scoremaster_Presentation.Models
+{
+    public class RubricCreate
+    {
+        public int RubricCreateId { get; set; }
+        public string RubricName { get; set; }
+        public DateTime Date { get; set; }
+        public bool Isactive { get; set; }
+        public bool IsDeleted { get; set; }
+    }
+}
