169ede99453d7ffbc67e7eeab743f54923b4fe91
M.Ismail <miktv101@gmail.com>
2024-03-15 16:48:04 +0500

    change password

--- scoremaster Presentation/Controllers/RegistrationController.cs
+++ scoremaster Presentation/Controllers/RegistrationController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using scoremaster_Presentation.Data;
 using scoremaster_Presentation.Models;
+using System.Security.Claims;
 
 namespace scoremaster_Presentation.Controllers
 {
@@ -41,5 +42,45 @@ namespace scoremaster_Presentation.Controllers
             _context.SaveChanges();
             return RedirectToAction(nameof(UserRegisterCreate));
         }
+        [HttpGet]
+        public IActionResult UserChangePassword( string Message1)
+        {
+            ViewBag.Message = Message1;
+            var user = User.FindFirst(ClaimTypes.Sid)?.Value;
+            int uIdint = Convert.ToInt32(user);
+            var Changepass=_context.UsersRegistrations.Where(x=>x.UsersRegistrationId==uIdint).FirstOrDefault();
+            ViewBag.UserId = Changepass.UsersRegistrationId;
+           ViewBag.password = Changepass.Password;
+            return View(Changepass);
+        }
+        [HttpPost]
+        public IActionResult UserChangePassword(string NewPassword, int Id)
+        {
+            var Message="";
+            var Changepass = _context.UsersRegistrations.FirstOrDefault(x => x.UsersRegistrationId == Id);
+            if (Changepass != null)
+            {
+                Changepass.Password = NewPassword;
+                try
+                {
+                    _context.UsersRegistrations.Attach(Changepass);
+                    _context.Entry(Changepass).State = EntityState.Modified;
+                    _context.SaveChanges(); // Fixed method name
+
+                      Message = "Password is changed, New paasword is "+ NewPassword;
+                }
+                catch (Exception ex)
+                {
+                    // Handle the exception appropriately, e.g., log the error or provide user feedback
+                    ViewBag.ErrorMessage = "An error occurred while saving changes: " + ex.Message;
+                }
+            }
+            else
+            {
+                ViewBag.ErrorMessage = "User not found!";
+            }
+            return RedirectToAction(nameof(UserChangePassword) ,new {Message1=  Message });
+        }
+
     }
 }
